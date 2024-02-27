cf0301c16a4540ed6096a60b2adf39307ded19b7
ADEAARAOMO <abdulkareemrukayat16@gmail.com>
2024-02-27 13:00:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,130 @@
+/**/
+console.log("Hello World")
+let name="Ruqayah";
+console.log(name);
+name="Ajoke";
+console.log(name);
+let age = 57;
+let newAge = 13;
+let percentage = newAge/age
+let myString = "hello";
+let otherString = "hi";
+let backstring = "hi again";
+let num = 33;
+let bol = true;
+let undefinedVariable;
+let emptyValue = null;
+console.log(myString);
+console.log(otherString);
+console.log(backstring);
+console.log(num);
+console.log(bol);
+console.log(undefinedVariable);
+console.log(emptyValue);
+console.log(percentage);
+console.log(typeof myString);
+console.log(typeof otherString);
+console.log(typeof backstring);
+
+var test ="testing"
+console.log(test);
+var test ="hello"
+console.log(test);
+
+let myName = "Abdulkareem Ruqayah";
+myName = "Abdulkareem Ruqayah"
+console.log(myName);
+ 
+//string Methods
+
+let string = "javascript";
+firstLetter = string[3]
+console.log(firstLetter);
+console.log(string.length);
+console.log(string.length - 1);
+console.log(string.toLowerCase());
+console.log(string.toUpperCase());
+
+let text = "programming";
+console.log(text.substring(1,4));
+console.log(text.substr(1,4));
+
+let day = "30 Days of Javascript";
+console.log(day.split());
+console.log(day.split(" "));
+console.log(day.includes(days));
+
+let title = "Javascript";
+let lastIndex = title.length - 1;
+console.log(title.charAt(0));
+console.log(title.indexOf("j"));
+console.log(day.replace("days","Months"));
+
+//Assignment Operators
+let numOne = 5;
+let numTwo = 4;
+let sum = numone + numTwo;
+let diff = numOne - numTwo;
+let mult = numOne * numTwo;
+let div = numOne / numTwo;
+let remainder = numOne / numTwo;
+let powerOf = numOne ** numTwo;
+
+//Comparison Operator
+let first = 3;
+let second = 2;
+
+console.log(first > second);
+console.log(first < second);
+console.log(first == second);
+console.log(first === second);
+console.log(first===! second);
+console.log(first <= second); 
+
+console.log("3"== 3);
+console.log("3"=== 3);
+
+//console.log()
+console.log(1 ==true);
+console.log(0 == true);
+console.log(0 == false);
+console.log("0" === false);
+
+//logical Operators
+let check = 4 > 3 && 10 > 5;
+let see =4 > 3 && 5 > 10;
+console.log(check, see);
+
+//true and true = true
+//true and false = false
+//fase and false = false
+
+let compare = 4 > 3 || 10 > 5;
+let value = 4 > 3 || 10 < 5 ;
+let answer = 4 < 3 ||  10 < 5;
+console.log(compare, value, answer);
+
+//negation operators 
+//let negate =|4 < 3;
+//let result =|true;
+//console.log(negate, result);
+
+
+
+//increment and decrement
+let count = 20;
+
+//console.log(--num);
+console.log(num--);
+console.log(num);
+
+//Ternary operators
+let isMan = true;
+ console.log("yes, he is a man");
+console.log("No, he is not a man");
+
+
+
+
+
+
